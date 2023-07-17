c4ae46c8b6f9a614b918fc328be8984841624044
Yu <yiyu@bamdev1.elzar.cshl.edu>
2023-07-17 17:43:21 -0400

    update

---
+++ data/download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# wget hg19.fa
+# wget https://www.dropbox.com/s/mojkzvlmsw2bk6b/hg19.fa
+
+# wget gencode
+# wget https://www.dropbox.com/s/1j8imw5eykjyzvm/gencode.v43lift37.basic.annotation.gtf.gz
+# gunzip gencode.v43lift37.basic.annotation.gtf.gz
+
+# wget DeepSTARR dataset
+wget https://zenodo.org/record/7265991/files/DeepSTARR_data.h5
