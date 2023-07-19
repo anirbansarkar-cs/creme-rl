4f78168d5d53676b5a870caf579321dafa350d4b
Steven Yu <2322740290@qq.com>
2023-07-18 22:26:41 -0400

    udpate

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="creme-rl",
+    version="0.1.0",
+    packages=find_packages(),
+    description = "A RL package in genomics with infinite potentials ",
+    python_requires=">=3.6",
+    install_requires=[],
+)
